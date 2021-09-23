8b83a8cf13400fd3a8f73e8bfba63f0b269bd6c7
JosueMosquera <82737793+JosueMosquera@users.noreply.github.com>
2021-09-23 17:44:37 -0500

    Webserver

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT;
+//handlebars
+app.set('view engine','hbs')
+hbs.registerPartials(__dirname+'/views/partials',(err)=>{console.log(err)})
+
+//TODO servir contenido estatico
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+   res.render('home',{
+       nombre:'Josue Mosquera',
+       titulo:'Curso de Node'
+   });
+})
+app.get('/generic',(req,res)=>{
+    res.render('generic')
+})
+app.get('/elements',(req,res)=>{
+    res.render('elements');
+})
+app.get('*',(req,res)=>{
+    res.render('404');
+})
+app.listen(8081);
+
